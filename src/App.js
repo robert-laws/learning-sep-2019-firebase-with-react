bc806cc7e1d01beaf3929cdfedbbb542e2a59256
Robert Laws <roblaws@me.com>
2019-09-16 17:00:41 +0300

    test animation

--- src/App.js
+++ src/App.js
@@ -2,16 +2,37 @@ import React from 'react';
 
 import './App.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>
-          Firebase with React
-        </h1>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      hidden: false
+    }
+  }
+
+  hideBox = () => {
+    console.log('clicked...')
+    this.setState({
+      hidden: !this.state.hidden
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>
+            Firebase with React
+          </h1>
+          <div className={`box ${this.state.hidden ? 'fade-out' : 'fade-in'}`}>
+            <span>Hello There...</span>
+          </div>
+          <button onClick={this.hideBox}>Click Here</button>
+        </header>
+      </div>
+    )
+  }
 }
 
 export default App;
