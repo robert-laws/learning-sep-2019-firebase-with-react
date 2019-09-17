911c88aef56a2257aada7d76153530837b987151
Robert Laws <roblaws@me.com>
2019-09-17 11:53:16 +0300

    update styles/import bootstrap css

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { render } from 'react-dom';
 import Application from './components/Application';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.scss';
 
 render(<Application />, document.getElementById('root'));
